634d340cc1df70e929b03382dee702b955adfe29
Emmanuel Thompson <eet6646@gmail.com>
2021-06-29 13:24:05 -0400

    feat: Implement Ipv6 finalize
    Update the checksum field

--- src/layer/ip/ipv6.rs
+++ src/layer/ip/ipv6.rs
@@ -1,11 +1,13 @@
 /*!
   Ipv6
+
+  TODO: Ipv6 extension headers
 */
 
-use crate::layer::{Layer, LayerError, LayerExt, LayerOwned};
-
 use super::IpProtocol;
-use alloc::{format, vec::Vec};
+use crate::layer::{Layer, LayerError, LayerExt, LayerOwned};
+use alloc::{format, string::ToString, vec::Vec};
+use core::convert::TryFrom;
 use deku::prelude::*;
 
 /**
@@ -64,19 +66,6 @@ pub struct Ipv6 {
     pub dst: u128,
 }
 
-impl Ipv6 {
-    //pub fn update_length(&mut self, data: &[Layer]) -> Result<(), LayerError> {
-    //let mut data_buf = Vec::new();
-    //for layer in data {
-    //data_buf.extend(layer.to_bytes()?)
-    //}
-
-    //self.length = u16::try_from(data_buf.len())?;
-
-    //Ok(())
-    //}
-}
-
 impl Default for Ipv6 {
     fn default() -> Self {
         Ipv6 {
@@ -95,9 +84,15 @@ impl Default for Ipv6 {
 
 impl Layer for Ipv6 {}
 impl LayerExt for Ipv6 {
-    fn finalize(&mut self, _prev: &[LayerOwned], _next: &[LayerOwned]) -> Result<(), LayerError> {
-        // TODO: Update length
-        todo!()
+    fn finalize(&mut self, _prev: &[LayerOwned], next: &[LayerOwned]) -> Result<(), LayerError> {
+        // Update length field
+        self.length =
+            u16::try_from(crate::layer::utils::length_of_layers(next)?).map_err(|_e| {
+                LayerError::Finalize("Could not convert layer length to u16".to_string())
+            })?;
+
+        // TODO: Update next header?
+        Ok(())
     }
 
     fn parse(input: &[u8]) -> Result<(&[u8], Self), LayerError>
@@ -117,9 +112,51 @@ impl LayerExt for Ipv6 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::layer::{Layer, LayerError, LayerExt};
+    use alloc::boxed::Box;
     use hexlit::hex;
     use rstest::*;
     use std::convert::TryFrom;
+
+    macro_rules! declare_test_layer {
+        ($name:ident, $size:tt) => {
+            #[derive(Debug, Default)]
+            struct $name {}
+            #[allow(dead_code)]
+            impl $name {
+                fn new() -> Self {
+                    Self {}
+                }
+                fn boxed() -> Box<dyn LayerExt> {
+                    Box::new(Self {})
+                }
+            }
+            impl Layer for $name {}
+            impl LayerExt for $name {
+                fn finalize(
+                    &mut self,
+                    _prev: &[LayerOwned],
+                    _next: &[LayerOwned],
+                ) -> Result<(), LayerError> {
+                    unimplemented!()
+                }
+
+                fn parse(_input: &[u8]) -> Result<(&[u8], Self), LayerError>
+                where
+                    Self: Sized,
+                {
+                    unimplemented!()
+                }
+
+                fn to_vec(&self) -> Result<Vec<u8>, LayerError> {
+                    Ok([0u8; $size].to_vec())
+                }
+            }
+        };
+    }
+
+    declare_test_layer!(Layer0, 0);
+    declare_test_layer!(Layer100, 100);
 
     #[rstest(input, expected,
         case(
@@ -158,5 +195,37 @@ mod tests {
             },
             Ipv6::default(),
         );
+    }
+
+    #[rstest(expected_length, layers,
+        case::none(0, &[]),
+        case::empty(0, &[Layer0::boxed()]),
+        case::empty(100, &[Layer100::boxed()]),
+        case::empty(200, &[Layer100::boxed(), Layer0::boxed(), Layer100::boxed()]),
+    )]
+    fn test_ipv6_finalize_length(expected_length: u16, layers: &[LayerOwned]) {
+        let mut ipv6 = Ipv6::default();
+        assert_eq!(0, ipv6.length);
+
+        // Finalize should update the length
+        ipv6.finalize(&[], layers).unwrap();
+
+        assert_eq!(expected_length, ipv6.length);
+    }
+
+    #[test]
+    fn test_ipv6_finalize() {
+        let mut ipv6 = Ipv6::default();
+        assert_eq!(0, ipv6.length);
+
+        ipv6.finalize(&[Layer100::boxed()], &[Layer100::boxed()])
+            .unwrap();
+
+        // Only these fields should change during a finalize
+        let expected_ipv6 = Ipv6 {
+            length: 100,
+            ..Default::default()
+        };
+        assert_eq!(expected_ipv6, ipv6);
     }
 }
